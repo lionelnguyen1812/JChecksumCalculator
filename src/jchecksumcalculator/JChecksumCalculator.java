fe313a294e11f880c76e191ef36f6c72f8c3bcce
Mr. Fukugami Makoto <lionel.nguyen1812@gmail.com>
2013-08-19 23:02:02 +0700

    commit

--- src/jchecksumcalculator/JChecksumCalculator.java
+++ src/jchecksumcalculator/JChecksumCalculator.java
@@ -1,8 +1,6 @@
 /* @author chad */
 package jchecksumcalculator;
 
-import jfilechecksumverifier.JFCV;
-
 public class JChecksumCalculator {
 
     public static void main(String[] args) {
--- src/jchecksumcalculator/JFCV.java
+++ src/jchecksumcalculator/JFCV.java
@@ -1,4 +1,4 @@
-package jfilechecksumverifier;
+package jchecksumcalculator;
 
 import java.io.BufferedInputStream;
 import java.io.File;
